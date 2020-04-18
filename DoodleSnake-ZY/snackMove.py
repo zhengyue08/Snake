3b0d11e1784525c2d25f152d2a8aa1e53fbfd4d8
Ibroad <57664875+Ibroad@users.noreply.github.com>
2020-04-19 06:38:00 +0800

    Achieve the movement of Snake

--- DoodleSnake-ZY/snackMove.py
+++ DoodleSnake-ZY/snackMove.py
@@ -1,45 +1,75 @@
 from turtle import *
+import threading
+unit = 20
 snake = Turtle()
-snake_list = [(0,100),(20,100),(40,100),(60,100),(80,100)]
+screen = Screen()
+snake_list = [(0,0),(0,unit),(0,unit*2),(0,unit*3),(0,unit*4),(0,unit*5)]
 stamp_list = []
-aaakdlgl
+snake_dir="up"
+
 def drawSnake(snake_list):
     snake.hideturtle()
     tracer(0)
-    snake.penup()
-    snake.goto(snake_list[0])
     snake.shape("square")
     snake.pencolor("red")
-    snake.pendown()
     for i in snake_list:
         if i==snake_list[-1]:
             snake.penup()
             snake.fillcolor("red")
             snake.goto(i)
             snake.pendown()
-            # snake.stamp()
-            a=snake.stamp()
-            stamp_list.append(a)
+            n=snake.stamp()
+            stamp_list.append(n)
         else:
             snake.penup()
+            snake.fillcolor("black")
             snake.goto(i)
             snake.pendown()
-            # snake.stamp()
-            a = snake.stamp()
-            stamp_list.append(a)
+            n = snake.stamp()
+            stamp_list.append(n)
     update()
-drawSnake(snake_list)
-# # del snake_list[0]
-# # snake_list.append((100,100))
-# # snake.clearstamp(3)
-# print(stamp_list)
-snake.clearstamp(stamp_list[-1])
 
 
+def goUp():
+    global snake_dir
+    snake_dir = "up"
+def goRight():
+    global snake_dir
+    snake_dir = "right"
 
 
+def goDown():
+    global snake_dir
+    snake_dir = "down"
+
+def goLeft():
+    global snake_dir
+    snake_dir = "left"
+def control():
+    screen.listen()
+    screen.onkey(goLeft,"Left")
+    screen.onkey(goRight, "Right")
+    screen.onkey(goUp, "Up")
+    screen.onkey(goDown, "Down")
 
 
+def mainMove():
+    drawSnake(snake_list)
+    snake.clearstamps()
+    del snake_list[0]
+    control()
+    if snake_dir=="right":
+        snake_list.append((snake_list[-1][0] + unit, snake_list[-1][1]))
+    if snake_dir=="left":
+        snake_list.append((snake_list[-1][0] - unit, snake_list[-1][1]))
+    if snake_dir=="up":
+        snake_list.append((snake_list[-1][0] , snake_list[-1][1]+unit))
+    if snake_dir=="down":
+        snake_list.append((snake_list[-1][0] , snake_list[-1][1]-unit))
+    drawSnake(snake_list)
+    screen.ontimer(mainMove,250)
+
+mainMove()
 
 
 done()
